4d28d8a328670eb2b3e2352f372249b311dfab8b
itujun <itujun7@gmail.com>
2025-03-03 09:26:01 +0700

    form validation with useFormState hook (ketika Create data)

--- src/app/product-db-create/page.tsx
+++ src/app/product-db-create/page.tsx
@@ -1,44 +1,59 @@
+'use client';
+
+import { addProduct, FormState } from '@/actions/products';
 import { SubmitButton } from '@/components/SubmitButton';
-import { createProduct } from '@/prisma-db';
-import { redirect } from 'next/navigation';
+import { useActionState } from 'react';
 
 export default function ProductDBCreatePage() {
-  async function creatingProduct(formData: FormData) {
-    'use server';
+  const initialState: FormState = {
+    errors: {},
+  };
 
-    const title = formData.get('title') as string;
-    const price = formData.get('price') as string;
-    const description = formData.get('description') as string;
-
-    await createProduct(title, parseInt(price), description);
-    redirect('/products-db');
-  }
+  const [state, formAction, isPending] = useActionState(
+    addProduct,
+    initialState
+  );
   return (
-    <form action={creatingProduct} className="p-4 space-y-4 max-w-96">
-      <label htmlFor="title" className="text-white">
-        Title
-      </label>
-      <input
-        type="text"
-        name="title"
-        className="block w-full p-2 bg-white text-black border rounded"
-      />
-      <label htmlFor="price" className="text-white">
-        Price
-      </label>
-      <input
-        type="number"
-        name="price"
-        className="block w-full p-2 bg-white text-black border rounded"
-      />
-      <label htmlFor="description" className="text-white">
-        Description
-      </label>
-      <input
-        name="description"
-        type="text"
-        className="block w-full p-2 bg-white text-black border rounded"
-      />
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <div>
+        <label htmlFor="title" className="text-white">
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="block w-full p-2 bg-white text-black border rounded"
+        />
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="price" className="text-white">
+          Price
+        </label>
+        <input
+          type="number"
+          name="price"
+          className="block w-full p-2 bg-white text-black border rounded"
+        />
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="description" className="text-white">
+          Description
+        </label>
+        <input
+          name="description"
+          type="text"
+          className="block w-full p-2 bg-white text-black border rounded"
+        />
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
       {/* <button
         type="submit"
         className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500"
